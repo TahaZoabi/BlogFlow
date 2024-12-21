2b73c904c46e29c7dda903048bb902b3f1933eb9
Taha Zoabi <zoabi03@gmail.com>
2024-12-21 09:46:22 +0200

    setup shadcn

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
 <template>
-  <div class="flex h-screen justify-center items-center">
+  <div class="flex h-screen justify-center items-center flex-col gap-2">
     <h1 class="text-green-600 text-4xl">Hello World</h1>
+    <Button>Welcome</Button>
   </div>
 </template>
