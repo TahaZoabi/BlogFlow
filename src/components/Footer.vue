ee0140cf7b86a639cba8f63bbd5e2b8dbebbf151
Taha Zoabi <zoabi03@gmail.com>
2024-12-22 10:21:43 +0200

    .

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,11 +5,6 @@ const year = time.getFullYear()
 
 <template>
   <div class="flex flex-col items-center justify-center gap-4">
-    <ul class="flex justify-center items-center gap-5">
-      <li>About</li>
-      <li>Privacy Policy</li>
-      <li>Terms of Service</li>
-    </ul>
     <h5>© {{ year }} Taha Zoabi. All rights reserved.</h5>
   </div>
 </template>
