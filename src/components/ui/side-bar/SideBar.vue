70254629b65bf2bd0e4e62ac08e7aae0b77192d6
Taha Zoabi <zoabi03@gmail.com>
2024-12-23 11:38:50 +0200

    added a side bar

---
+++ src/components/ui/side-bar/SideBar.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { Github, Linkedin, Mail } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="hidden md:block w-1/4">
+    <!-- Sidebar -->
+    <div class="flex flex-col h-full p-4 gap-20">
+      <div>
+        <h5 class="text-xl mb-2">Categories:</h5>
+        <ul class="flex flex-col gap-3 p-2 justify-center">
+          <li>Tech</li>
+          <li>Tools</li>
+          <li>Tutorials</li>
+        </ul>
+      </div>
+      <div>
+        <h5 class="text-xl mb-2">Recent Posts:</h5>
+        <ul class="flex flex-col gap-3 p-2 justify-center">
+          <li>Title One</li>
+          <li>Title Two</li>
+          <li>Title Three</li>
+        </ul>
+      </div>
+      <div>
+        <h5 class="text-xl mb-2">Social Media:</h5>
+        <ul class="flex gap-3 p-2 justify-start items-center">
+          <li class="hover:text-primary">
+            <a href="mailto:range.dev7@gmail.com"><Mail /></a>
+          </li>
+          <li class="hover:text-primary"><Github /></li>
+          <li class="hover:text-primary"><Linkedin /></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { SideBar } from '@/components/ui/side-bar'
+</script>
 
 <template>
-  <main>
-    <h2>Home Page</h2>
-  </main>
+  <div class="flex h-screen w-full my-3">
+    <!-- Sidebar -->
+    <SideBar />
+
+    <!-- Main Content -->
+    <main class="flex-grow p-4">
+      <h1 class="text-3xl font-semibold">Welcome to the Homepage</h1>
+      <p>This is the main content area.</p>
+    </main>
+  </div>
 </template>
