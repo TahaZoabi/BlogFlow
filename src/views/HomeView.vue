19f4ae28b5572d3a73fdd1db2fd045d134634a97
Taha Zoabi <zoabi03@gmail.com>
2024-12-24 11:22:46 +0200

    main grid layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@ import BlogPost from '@/components/BlogPost.vue'
 </script>
 
 <template>
-  <div class="flex flex-col md:flex-row h-screen w-full my-3">
+  <div class="flex flex-col md:flex-row min-h-screen w-full my-3">
     <!-- Sidebar -->
     <SideBar class="hidden md:block" />
     <MobileSideBar />
@@ -13,7 +13,9 @@ import BlogPost from '@/components/BlogPost.vue'
     <main class="flex-grow p-4">
       <h1 class="text-2xl font-semibold">Welcome to the Homepage</h1>
       <p>This is the main content area.</p>
-      <div>
+      <div class="grid grid-cols-1 md:grid-cols-2 my-4 gap-8">
+        <BlogPost />
+        <BlogPost />
         <BlogPost />
       </div>
     </main>
