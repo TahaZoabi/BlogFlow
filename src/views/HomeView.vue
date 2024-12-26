20a26e30d1369aa7a542f0fe7837b24a2f1baa78
Taha Zoabi <zoabi03@gmail.com>
2024-12-26 16:43:47 +0200

    layout fix (css)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,24 +6,22 @@ import BlogPost from '@/components/BlogPost.vue'
 </script>
 
 <template>
-  <div class="flex flex-col md:flex-row min-h-screen w-full my-3">
+  <main class="flex flex-col md:flex-row min-h-screen w-full my-3">
     <!-- Sidebar -->
     <SideBar class="hidden md:block" />
     <MobileSideBar />
     <!-- Main Content -->
-    <main class="flex-grow p-4">
+    <section class="flex-grow p-4">
       <h1 class="text-2xl font-semibold">Discover the Latest Stories</h1>
       <p class="text-sm text-muted-foreground">
         Welcome to our blog! We share thoughtful posts on Web Development, including everything from
         expert advice to personal stories. Check out our latest articles below!
       </p>
-      <div
-        v-for="post in BlogPosts"
-        :key="post.id"
-        class="grid grid-cols-1 md:grid-cols-2 my-4 gap-8"
-      >
-        <BlogPost :post="post" />
+      <div class="grid grid-cols-1 md:grid-cols-2 my-4 gap-8">
+        <div v-for="post in BlogPosts" :key="post.id">
+          <BlogPost :post="post" />
+        </div>
       </div>
-    </main>
-  </div>
+    </section>
+  </main>
 </template>
