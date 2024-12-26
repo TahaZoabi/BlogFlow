3e96921cd3dcbc6eb7fcc8f4b76d35b0927c534c
Taha Zoabi <zoabi03@gmail.com>
2024-12-26 14:55:40 +0200

    not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import ContactView from '@/views/ContactView.vue'
 import BlogPostView from '@/views/BlogPostView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
       path: '/posts/:id',
       name: 'blog post',
       component: BlogPostView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFoundView,
     },
   ],
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { TriangleAlert } from 'lucide-vue-next'
+</script>
+
+<template>
+  <section class="flex flex-col gap-4 justify-center items-center h-screen">
+    <div><TriangleAlert class="size-[150px] text-yellow-400" /></div>
+    <h2 class="text-4xl">404 Not Found</h2>
+    <p class="text-muted-foreground text-xl">this page does not exist</p>
+    <Button><RouterLink to="/">Go Back</RouterLink></Button>
+  </section>
+</template>
+
+<style scoped></style>
