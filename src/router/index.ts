b3a21331a38b2770ac0ff9a247396459260dd107
Taha Zoabi <zoabi03@gmail.com>
2024-12-30 12:34:03 +0200

    routes updated

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import ContactView from '@/views/ContactView.vue'
 import BlogPostView from '@/views/BlogPostView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
+  end: undefined,
+  sensitive: undefined,
+  strict: undefined,
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
