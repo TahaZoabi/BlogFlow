d793164500f13b130afa654b6ca33415c40b41c0
Taha Zoabi <zoabi03@gmail.com>
2024-12-30 12:51:41 +0200

    routes fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,40 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import AboutView from '@/views/AboutView.vue'
 import ContactView from '@/views/ContactView.vue'
 import BlogPostView from '@/views/BlogPostView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import HomeView from '@/views/HomeView.vue'
 
+import type { RouteRecordRaw } from 'vue-router'
+
+const basicRoutes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView,
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView,
+  },
+  ,
+  {
+    path: '/posts/:id',
+    name: 'blog-post',
+    component: BlogPostView,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: NotFoundView,
+  },
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView,
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: ContactView,
-    },
-    ,
-    {
-      path: '/posts/:id',
-      name: 'blog-post',
-      component: BlogPostView,
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'not-found',
-      component: NotFoundView,
-    },
-  ],
+  history: createWebHashHistory(),
+  routes: basicRoutes as RouteRecordRaw[],
 })
 
 export default router
