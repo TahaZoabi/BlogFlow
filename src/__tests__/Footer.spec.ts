30dd31541c37b6be71359b24791eb20bb5578624
Taha Zoabi <zoabi03@gmail.com>
2024-12-31 16:48:41 +0200

    footer tests

--- src/__tests__/Footer.spec.ts
+++ src/__tests__/Footer.spec.ts
@@ -7,4 +7,14 @@ describe('Footer Component', () => {
   it('should render correctly', () => {
     expect(wrapper.html()).toMatchSnapshot()
   })
+
+  it('should render the correct text', () => {
+    expect(wrapper.text()).toMatch(/taha zoabi/i)
+  })
+
+  it('should render the correct year', () => {
+    const year = new Date().getFullYear()
+    expect(wrapper.text()).toMatch(year.toString())
+  })
 })
+
